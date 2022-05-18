7673d56796e5285ee5257b489afc8435e35e4c62
Clayton S <desertcow07@gmail.com>
2022-05-18 09:53:24 -0700

    Password Gen working and dumping to console, missing taking user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,50 @@
-// Assignment code here
-var passwordLength = 10;
+// Varriable Decleration
+
+var Debug = false;
+
+var password = {
+  newLength: 10,
+  lowercase: true,
+  uppercase: false,
+  numeric: true,
+  specialChar: true
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword(password.newLength));
+
+console.log("Password Length = " + password.newLength);
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword(passwordLength);
+function writePassword(newLength) {
+  var password = "TEMP";
+  console.log("Password Length = " + newLength);
+  password = generatePassword(newLength);
+
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
+  console.log("New Password = " + password);
 }
 
-function generatePassword(passwordLength) {
-  var newPassword = '';
+function generatePassword(passLength) {
+  var finalPassword = '';
   var valid_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()"
 
-  for (let i = 0; i < passwordLength; i++) {
-    newPassword = newPassword + valid_chars.charAt(Math.floor(Math.random() * valid_chars.length));
-    console.log("i = " + i + "|| passwordLength =" + passwordLength)
-  }
-  return newPassword;
+  for (let i = 0; i < passLength; i++) {
+    finalPassword = finalPassword + valid_chars.charAt(Math.floor(Math.random() * valid_chars.length));
+    if (Debug) {
+      console.log("i = " + i + "|| passwordLength =" + passLength)
+    }
 
-  console.log("New Password: " + newPassword);
+  }
+  return finalPassword;
+
+  console.log("New Password: " + finalPassword);
 }
+
+
+console.log("New Password = " + generatePassword(password.newLength));
 
